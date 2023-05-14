795dd6f860e2ba2c2905d7d16b67cab2dd176843
suis_chun <48282517+suis-0@users.noreply.github.com>
2023-05-15 08:13:51 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Suis on 2023/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
